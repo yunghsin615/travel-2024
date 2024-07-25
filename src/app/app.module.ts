17c86dc893eb2e87a2acfcc4fdde644393db6bc2
kaydence <duo880615@gmail.com>
2024-07-25 15:08:11 +0800

    create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SideBarComponent } from './shared/side-bar/side-bar.component';
+import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
+import { InformationComponent } from './shared/information/information.component';
+import { MyFavsComponent } from './my-favs/my-favs.component';
+import { DataListComponent } from './data-list/data-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SideBarComponent,
+    NavBarComponent,
+    InformationComponent,
+    MyFavsComponent,
+    DataListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/my-favs/my-favs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-favs',
+  templateUrl: './my-favs.component.html',
+  styleUrls: ['./my-favs.component.scss']
+})
+export class MyFavsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
