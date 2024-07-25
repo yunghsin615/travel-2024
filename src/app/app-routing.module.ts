ff981eea8fe9f6960c72f6eb0968ca529c5a8f5a
kaydence <duo880615@gmail.com>
2024-07-25 16:23:01 +0800

    建立兩個頁面layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DataListComponent } from './data-list/data-list.component';
+import { MyFavsComponent } from './my-favs/my-favs.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'data-list', pathMatch: 'full' },
+  { path: 'data-list', component: DataListComponent },
+  { path: 'my-favs', component: MyFavsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
